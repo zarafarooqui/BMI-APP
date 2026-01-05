8c15fb64ea9ff72679ec11ac921405ebc398c516
zarafarooqui <engrimsha@gmail.com>
2026-01-05 18:27:02 +0500

    bmi calculator

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask , request , jsonify
+from flask import Flask , render_template , request , jsonify
 
 app=Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
 @app.route('/bmi', methods=["POST"])
 def calculate_bmi():
     data = request.get_json()
 
     weight = data['weight']
-    height = data["height"]
+    height = data['height']
 
-    bmi = weight*height
+    bmi = weight/(height**2)
 
     return jsonify({
         "BMI":round(bmi,2)
